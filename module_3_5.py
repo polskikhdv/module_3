7a95a6f41eed153d2b83ed57d3b370b532601c13
polskikhdv <polskikhdv@icloud.com>
2024-11-17 17:28:33 +0500

    done

--- module_3_5.py
+++ module_3_5.py
@@ -1,6 +1,6 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    if len(str_number) == 1:  # Базовый случай: одна цифра
+    if len(str_number) == 1:
         return int(str_number)
     first = int(str_number[0])
     return first * get_multiplied_digits(int(str_number[1:]))
