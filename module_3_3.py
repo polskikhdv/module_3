30172e49d96074dd312b2f6767805dedde842a6c
polskikhdv <polskikhdv@icloud.com>
2024-11-17 10:25:53 +0500

    done

--- module_3_3.py
+++ module_3_3.py
@@ -1,9 +1,5 @@
-from idlelib.editor import keynames
-
-
-def print_params(a = 1, b = 'строка', c = True):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
-
 
 
 print_params()
@@ -11,7 +7,7 @@ print_params(True, 2, (1, 3, 5))
 print_params(c=9)
 print_params('строчка', b=9)
 values_list = [1, False, 'String']
-values_dict = {'a':'String', 'b': 5.0, 'c': 7}
+values_dict = {'a': 'String', 'b': 5.0, 'c': 7}
 print_params(*values_list)
 print_params(values_list)
 print_params(values_dict)
@@ -19,5 +15,3 @@ print_params(*values_dict)
 values_list_2 = [7, "sr"]
 print_params(values_list_2)
 print_params(*values_list_2, 42)
-
-
